79b9698915faec59be0f436a57d09341d2ca27ff
TAKTY-MBP-ST\Takty <tokutadaginaya@gmail.com>
2018-05-02 17:37:47 +0900

    Modify packages

--- gulpfile.js
+++ gulpfile.js
@@ -12,32 +12,32 @@ var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('js', function () {
 	gulp.src(['src/js/basic/*.js', 'src/js/content/*.js'])
-	.pipe(plumber())
-	.pipe(babel({presets: ['es2015']}))
-	.pipe(concat('stile.min.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('dist/js'));
+		.pipe(plumber())
+		.pipe(babel({presets: ['es2015']}))
+		.pipe(concat('stile.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));
 
 	gulp.src('src/js/**/*.js')
-	.pipe(plumber())
-	.pipe(babel({presets: ['es2015']}))
-	.pipe(concat('stile-full.min.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('dist/js'));
+		.pipe(plumber())
+		.pipe(babel({presets: ['es2015']}))
+		.pipe(concat('stile-full.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));
 
 	gulp.src('src/js/*/*.js')
-	.pipe(plumber())
-	.pipe(babel({presets: ['es2015']}))
-	.pipe(uglify())
-	.pipe(rename({extname: '.min.js'}))
-	.pipe(gulp.dest('dist/js'));
+		.pipe(plumber())
+		.pipe(babel({presets: ['es2015']}))
+		.pipe(uglify())
+		.pipe(rename({extname: '.min.js'}))
+		.pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('sass', function () {
 	gulp.src(['src/sass/**'], {base: 'src/sass'})
-	.pipe(changed('dist/sass'))
-	.pipe(plumber())
-	.pipe(gulp.dest('dist/sass'));
+		.pipe(changed('dist/sass'))
+		.pipe(plumber())
+		.pipe(gulp.dest('dist/sass'));
 });
 
 gulp.task('watch', function() {
@@ -53,11 +53,17 @@ gulp.task('default', ['js', 'sass', 'watch']);
 
 gulp.task('sample', function () {
 	gulp.src('sample/style.scss')
-	.pipe(plumber())
-    .pipe(sourcemaps.init())
-	.pipe(sass())
-	.pipe(cleanCSS())
-	.pipe(rename({extname: '.min.css'}))
-	.pipe(sourcemaps.write('.'))
-	.pipe(gulp.dest('sample'));
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(sass())
+		.pipe(cleanCSS())
+		.pipe(rename({extname: '.min.css'}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('sample'));
+});
+
+gulp.task('dev-sample', function () {
+	gulp.watch('sample/**/*.scss', ['sample']);
+	gulp.watch('src/js/**/*.js', ['js']);
+	gulp.watch('src/sass/**/*.scss', ['sass', 'sample']);
 });
