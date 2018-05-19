df024e22ec9257ade5d4ca61245ffadd9ea71bd1
takty <tokutadaginaya@gmail.com>
2018-05-19 22:50:08 +0900

    Add card-* classes

---
+++ src/js/option/container.js
@@ -0,0 +1,42 @@
+/**
+ *
+ * Classes for Containers (JS)
+ *
+ * @author Takuto Yanagida @ Space-Time Inc.
+ * @version 2018-05-19
+ *
+ */
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+	const TARGET_SELECTOR = '.stile';
+
+	let cs = document.querySelectorAll(TARGET_SELECTOR + ' .card-3');
+	for (let i = 0; i < cs.length; i += 1) modifyCardStyle(cs[i], 3);
+	cs = document.querySelectorAll(TARGET_SELECTOR + ' .card-4');
+	for (let i = 0; i < cs.length; i += 1) modifyCardStyle(cs[i], 4);
+
+
+	// -------------------------------------------------------------------------
+	// Card
+
+	function modifyCardStyle(a, col) {
+		const cs = a.childNodes;
+		if (cs.length === 0) return false;
+
+		const parentTN = a.tagName;
+		let childTN = 'DIV';
+		switch (parentTN) {
+			case 'UL':
+			case 'OL':
+				childTN = 'LI';
+		}
+		for (let i = 0; i < col - 1; i += 1) {
+			const e = document.createElement(childTN);
+			a.appendChild(e);
+		}
+		return true;
+	}
+
+});
