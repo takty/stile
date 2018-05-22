e9156c79ce4b05a9fd7b50e02fa59b4e9d8f9727
TAKTY-MBP-ST\Takty <tokutadaginaya@gmail.com>
2018-05-22 17:37:50 +0900

    Refactor gulpfile and packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,76 @@
-var gulp    = require('gulp');
-var plumber = require('gulp-plumber');
-var changed = require('gulp-changed');
-var concat  = require('gulp-concat');
-var uglify  = require('gulp-uglify');
-var rename  = require('gulp-rename');
-var babel   = require('gulp-babel');
+'use strict';
 
-var sass         = require('gulp-sass');
-var cleanCSS     = require('gulp-clean-css');
-var sourcemaps   = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')({pattern:['gulp-*']});
 
-gulp.task('js', function () {
-	gulp.src(['src/js/basic/*.js', 'src/js/content/*.js'])
-		.pipe(plumber())
-		.pipe(babel({presets: ['es2015']}))
-		.pipe(concat('stile.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist/js'));
 
-	gulp.src('src/js/**/*.js')
-		.pipe(plumber())
-		.pipe(babel({presets: ['es2015']}))
-		.pipe(concat('stile-full.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist/js'));
-
-	gulp.src('src/js/*/*.js')
-		.pipe(plumber())
-		.pipe(babel({presets: ['es2015']}))
-		.pipe(uglify())
-		.pipe(rename({extname: '.min.js'}))
+gulp.task('js-with-option', function () {
+	return gulp.src('src/js/**/*.js')
+		.pipe($.plumber())
+		.pipe($.babel({presets: ['es2015']}))
+		.pipe($.concat('stile-full.min.js'))
+		.pipe($.uglify())
 		.pipe(gulp.dest('dist/js'));
 });
 
+gulp.task('js-without-option', function () {
+	return gulp.src(['src/js/basic/*.js', 'src/js/content/*.js'])
+		.pipe($.plumber())
+		.pipe($.babel({presets: ['es2015']}))
+		.pipe($.concat('stile.min.js'))
+		.pipe($.uglify())
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('js-each', function () {
+	return gulp.src('src/js/*/*.js')
+		.pipe($.plumber())
+		.pipe($.babel({presets: ['es2015']}))
+		.pipe($.uglify())
+		.pipe($.rename({extname: '.min.js'}))
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('js', ['js-with-option', 'js-without-option', 'js-each']);
+
 gulp.task('sass', function () {
-	gulp.src(['src/sass/**'], {base: 'src/sass'})
-		.pipe(changed('dist/sass'))
-		.pipe(plumber())
+	return gulp.src(['src/sass/**/*.scss'], {base: 'src/sass'})
+		.pipe($.plumber())
+		.pipe($.changed('dist/sass'))
 		.pipe(gulp.dest('dist/sass'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
 	gulp.watch('src/js/**/*.js', ['js']);
 	gulp.watch('src/sass/**/*.scss', ['sass']);
 });
 
 gulp.task('build', ['js', 'sass']);
-gulp.task('default', ['js', 'sass', 'watch']);
+
+gulp.task('default', ['build', 'watch']);
 
 
 // -----------------------------------------------------------------------------
 
-gulp.task('docs', function () {
-	gulp.src('docs/style.scss')
-		.pipe(plumber())
-		.pipe(sourcemaps.init())
-		.pipe(sass())
-		.pipe(cleanCSS())
-		.pipe(rename({extname: '.min.css'}))
-		.pipe(autoprefixer(['ie >= 11']))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('docs'));
-	gulp.src(['dist/js/stile-full.min.js'])
-		.pipe(plumber())
+gulp.task('docs-sass', ['sass'], function () {
+	return gulp.src('docs/style.scss')
+		.pipe($.plumber())
+		.pipe($.sourcemaps.init())
+		.pipe($.autoprefixer(['ie >= 11']))
+		.pipe($.sass({outputStyle: 'compressed'}))
+		.pipe($.rename({extname: '.min.css'}))
+		.pipe($.sourcemaps.write('.'))
 		.pipe(gulp.dest('docs'));
 });
 
-gulp.task('dev-docs', function () {
-	gulp.watch('docs/**/*.scss', ['docs']);
-	gulp.watch('src/js/**/*.js', ['js', 'docs']);
-	gulp.watch('src/sass/**/*.scss', ['sass', 'docs']);
+gulp.task('docs-js', ['js-with-option'], function () {
+	return gulp.src(['dist/js/stile-full.min.js'])
+		.pipe($.plumber())
+		.pipe(gulp.dest('docs'));
+});
+
+gulp.task('docs', ['default'], () => {
+	gulp.watch('src/js/**/*.js',     ['docs-js']);
+	gulp.watch('src/sass/**/*.scss', ['docs-sass']);
+	gulp.watch('docs/style.scss',    ['docs-sass']);
 });
