af72bb77431b2d1bbc5b1b228d3eac9c0011eb53
TAKTY-MBP-ST\Takty <tokutadaginaya@gmail.com>
2018-06-01 11:31:55 +0900

    Refactoring tab page

--- src/js/option/tab-page.js
+++ src/js/option/tab-page.js
@@ -3,7 +3,7 @@
  * Tab Page Classes (JS)
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2018-05-31
+ * @version 2018-06-01
  *
  */
 
@@ -13,87 +13,71 @@ let ST = ST || {};
 
 document.addEventListener('DOMContentLoaded', function () {
 
-	const SELECTOR_TARGET          = '.tab-page';
-	const CLS_TAB_LIST             = 'stile-tab-page-tab-list';
-	const CLS_TAB_LIST_SECOND      = 'low';
-	// const SELECTOR_TAB_LIST_SECOND = '.stile-tab-page-tab-list.low';
-	const ST_STATE_CURRENT         = 'current';
-
-	// let isAccordion = false;
-	// let currentIdx = 0;
+	const SELECTOR_TARGET    = '.tab-page';
+	const CLS_TAB_LIST       = 'stile-tab-page-tab-list';
+	const CLS_TAB_LIST_BELOW = 'stile-tab-page-tab-list-below';
+	const ST_STATE_CURRENT   = 'current';
 
 	const tabPages = [];
 	const tps = document.querySelectorAll(SELECTOR_TARGET);
 	for (let i = 0; i < tps.length; i += 1) {
-		const tabPage = createTab(tps[i]);
-		if (tabPage) {
-			addTabEvent(tabPage);
-			tabPages.push(tabPage);
-			onTabClick(0, tabPage);
-		}
+		const tabPage = createTabPage(tps[i]);
+		if (tabPage) tabPages.push(tabPage);
 	}
-	window.addEventListener('resize', function () {
-		for (let i = 0; i < tabPages.length; i += 1) onResize(tabPages[i]);
-	});
-	for (let i = 0; i < tabPages.length; i += 1) onResize(tabPages[i]);
+	window.addEventListener('resize', function () { onResize(tabPages); });
+	onResize(tabPages);
 
-	function createTab(container) {
+	function createTabPage(container) {
 		const fh = getFirstHeading(container);
 		if (!fh) return false;
 		const tabH = fh.tagName;
 
-		const pages = [], tabs = [];
-		let curPage = null;
-		let tabUl, tabUl2;
-
+		const pages = [], htmls = [];
 		const cs = [].slice.call(container.children);
+		let curPage = null;
 
 		for (let i = 0; i < cs.length; i += 1) {
 			const c = cs[i];
 			if (c.tagName === tabH) {
 				if (curPage) pages.push(curPage);
 				curPage = document.createElement('div');
-
 				container.removeChild(c);
-				const li = document.createElement('li');
-				li.innerHTML = c.innerHTML;
-				tabs.push(li);
+				htmls.push(c.innerHTML);
 			} else {
 				if (curPage) curPage.appendChild(c);
 			}
 		}
 		if (curPage) pages.push(curPage);
 
-		tabUl = document.createElement('ul');
-		tabUl.className = CLS_TAB_LIST;
-		for (let i = 0; i < tabs.length; i += 1) tabUl.appendChild(tabs[i]);
+		const tp = {pages, container, currentIdx: 0, isAccordion: false};
+		createTab(htmls, tp);
 
-		tabUl2 = tabUl.cloneNode(true);
-		tabUl2.classList.add(CLS_TAB_LIST_SECOND);
-		const tabs2 = [].slice.call(tabUl2.children);
-
-		container.insertBefore(tabUl, container.firstChild);
+		container.insertBefore(tp.tabUl, container.firstChild);
 		for (let i = 0; i < pages.length; i += 1) container.appendChild(pages[i]);
-		container.appendChild(tabUl2);
-		return {tabs, tabs2, pages, container, currentIdx: 0};
+		container.appendChild(tp.tabUl2);
+
+		addTabEvent(tp);
+		onTabClick(0, tp);
+		return tp;
 	}
 
-	function addTabEvent(tabPage) {
-		// console.log(tabPage);
-		for (let i = 0; i < tabPage.tabs.length; i += 1) {
-			const f = (function (idx) {
-				return function (e) {
-					e.preventDefault();
-					onTabClick(idx, tabPage);
-				};
-			})(i);
-			tabPage.tabs[i].addEventListener('click', f);
-			tabPage.tabs2[i].addEventListener('click', f);
+	function createTab(htmls, tp) {
+		tp.tabUl = document.createElement('ul');
+		for (let i = 0; i < htmls.length; i += 1) {
+			const li = document.createElement('li');
+			li.innerHTML = htmls[i];
+			tp.tabUl.appendChild(li);
 		}
+		tp.tabUl.className = CLS_TAB_LIST;
+		tp.tabs = [].slice.call(tp.tabUl.children);
+
+		tp.tabUl2 = tp.tabUl.cloneNode(true);
+		tp.tabUl2.className = CLS_TAB_LIST_BELOW;
+		tp.tabs2 = [].slice.call(tp.tabUl2.children);
 	}
 
-	function getFirstHeading(page) {
-		const cs = page.children;
+	function getFirstHeading(container) {
+		const cs = container.children;
 		for (let i = 0; i < cs.length; i += 1) {
 			const tn = cs[i].tagName;
 			if (tn[0] === 'H' && (tn[1] === '1' || tn[1] === '2' || tn[1] === '3' || tn[1] === '4' || tn[1] === '5' || tn[1] === '6')) {
@@ -103,88 +87,102 @@ document.addEventListener('DOMContentLoaded', function () {
 		return null;
 	}
 
-	// function filterChild(parent, nodeList) {
-	// 	return [].slice.call(nodeList).filter(function (x) {return x.parentNode === parent;});
-	// }
-
-	function onTabClick(idx, tabPage) {
-		const tabs = tabPage.tabs;
-		const tabs2 = tabPage.tabs2;
-		const pages = tabPage.pages;
-		if (tabPage.isAccordion) {
-			const visibleIdx = tabPage.currentIdx === idx ? -1 : idx;
-			if (visibleIdx === -1) {
-				for (let i = 0; i < tabs.length; i += 1) {
-					tabs[i].style.display = '';
-					tabs2[i].style.display = 'none';
-				}
-			} else {
-				for (let i = 0; i < tabs.length; i += 1) {
-					tabs[i].style.display = (i <= idx) ? '' : 'none';
-					tabs2[i].style.display = (i > idx) ? '' : 'none';
-				}
-			}
-			for (let i = 0; i < tabs.length; i += 1) {
-				if (i === visibleIdx) ST.addStile(tabs[i], ST_STATE_CURRENT);
-				else ST.removeStile(tabs[i], ST_STATE_CURRENT);
-				tabs[i].className = '';
-			}
-			for (let i = 0; i < pages.length; i += 1) {
-				if (i === visibleIdx) ST.addStile(pages[i], ST_STATE_CURRENT);
-				else ST.removeStile(pages[i], ST_STATE_CURRENT);
-				pages[i].className = '';
-			}
-			tabPage.currentIdx = visibleIdx;
-		} else {
-			for (let i = 0; i < tabs.length; i += 1) {
-				if (i === idx) ST.addStile(tabs[i], ST_STATE_CURRENT);
-				else ST.removeStile(tabs[i], ST_STATE_CURRENT);
-				tabs[i].className = '';
-			}
-			for (let i = 0; i < pages.length; i += 1) {
-				if (i === idx) ST.addStile(pages[i], ST_STATE_CURRENT);
-				else ST.removeStile(pages[i], ST_STATE_CURRENT);
-				pages[i].className = '';
-			}
-			tabPage.currentIdx = idx;
+	function addTabEvent(tp) {
+		const ts  = tp.tabs;
+		const ts2 = tp.tabs2;
+		for (let i = 0; i < ts.length; i += 1) {
+			const f = (function (idx) {
+				return function (e) {
+					e.preventDefault();
+					onTabClick(idx, tp);
+				};
+			})(i);
+			ts[i].addEventListener('click', f);
+			ts2[i].addEventListener('click', f);
 		}
 	}
 
-	function onResize(tabPage) {
-		tabPage.isAccordion = getComputedStyle(tabPage.tabs2[0].parentNode).display !== 'none';
+	function onTabClick(idx, tp) {
+		if (tp.isAccordion) {
+			idx = tp.currentIdx === idx ? -1 : idx;
+			updateAccordionTabState(idx, tp);
+		}
+		changeCurrentTab(idx, tp);
+	}
 
-		const tabs = tabPage.tabs;
-		const tabs2 = tabPage.tabs2;
-		// const pages = tabPage.pages;
+	function changeCurrentTab(idx, tp) {
+		const ts  = tp.tabs;
+		const ps  = tp.pages;
 
-		if (tabPage.isAccordion) {
-			tabPage.container.style.minHeight = 'initial';
-			tabPage.container.style.height = 'initial';
-			for (let i = 0; i < tabs.length; i += 1) {
-				tabs[i].style.display = (i <= tabPage.currentIdx) ? '' : 'none';
-				tabs2[i].style.display = (i > tabPage.currentIdx) ? '' : 'none';
+		for (let i = 0; i < ts.length; i += 1) {
+			if (i === idx) {
+				ST.addStile(ts[i], ST_STATE_CURRENT);
+				ST.addStile(ps[i], ST_STATE_CURRENT);
+			} else {
+				ST.removeStile(ts[i], ST_STATE_CURRENT);
+				ST.removeStile(ps[i], ST_STATE_CURRENT);
+			}
+			ts[i].className = '';
+			ps[i].className = '';
+		}
+		tp.currentIdx = idx;
+	}
+
+	function updateAccordionTabState(idx, tp) {
+		const ts  = tp.tabs;
+		const ts2 = tp.tabs2;
+		if (idx === -1) {
+			for (let i = 0; i < ts.length; i += 1) {
+				ts[i].style.display  = '';
+				ts2[i].style.display = 'none';
 			}
 		} else {
-			if (tabPage.currentIdx === -1) tabPage.currentIdx = 0;
-			const tabRow = tabPage.container.getElementsByTagName('ul')[0];
-			const pages = tabPage.pages;//filterChild(tabPage, tabPage.querySelectorAll('div'));
-
-			let margin = parseInt(getComputedStyle(tabRow).marginBottom);
-			let height = 0;
-
-			for (let j = 0; j < pages.length; j += 1) {
-				const p = pages[j];
-				margin = Math.max(margin, parseInt(getComputedStyle(p).marginTop));
-				height = Math.max(height, p.getBoundingClientRect().height);
+			for (let i = 0; i < ts.length; i += 1) {
+				ts[i].style.display  = (i <= idx) ? '' : 'none';
+				ts2[i].style.display = (i > idx)  ? '' : 'none';
 			}
-			tabPage.container.style.minHeight = tabRow.offsetHeight + margin + height + 'px';
-			tabPage.container.style.height = tabPage.container.style.minHeight;
-
-			for (let i = 0; i < tabs.length; i += 1) {
-				tabs[i].style.display = '';
-			}
-			onTabClick(tabPage.currentIdx, tabPage);
 		}
+	}
+
+	function onResize(tps) {
+		for (let i = 0; i < tps.length; i += 1) {
+			onResizeOne(tps[i]);
+		}
+	}
+
+	function onResizeOne(tp) {
+		const cont = tp.container;
+		tp.isAccordion = getComputedStyle(tp.tabUl2).display !== 'none';
+
+		if (tp.isAccordion) {
+			cont.style.minHeight = '';
+			cont.style.height    = '';
+			updateAccordionTabState(tp.currentIdx, tp);
+		} else {
+			const tabs  = tp.tabs;
+			const minH = getMinHeight(tp) + 'px';
+			cont.style.minHeight = minH;
+			cont.style.height    = minH;
+
+			for (let i = 0; i < tabs.length; i += 1) tabs[i].style.display = '';
+			if (tp.currentIdx === -1) tp.currentIdx = 0;
+			onTabClick(tp.currentIdx, tp);
+		}
+	}
+
+	function getMinHeight(tp) {
+		const tabUl = tp.tabUl;
+		const pages = tp.pages;
+
+		let margin = parseInt(getComputedStyle(tabUl).marginBottom);
+		let height = 0;
+
+		for (let i = 0; i < pages.length; i += 1) {
+			const p = pages[i];
+			margin = Math.max(margin, parseInt(getComputedStyle(p).marginTop));
+			height = Math.max(height, p.getBoundingClientRect().height);
+		}
+		return tabUl.offsetHeight + margin + height;
 	}
 
 });
