028030544ab669383c8805a1b5cc0c15bce7b4dc
Takuto Yanagida <yanagida@stxst.com>
2019-02-07 09:44:07 +0900

    Remove link style handling

--- src/js/content/content.js
+++ src/js/content/content.js
@@ -3,7 +3,7 @@
  * Content Style (JS)
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2019-02-02
+ * @version 2019-02-07
  *
  */
 
@@ -14,20 +14,9 @@ window.ST = window['ST'] || {};
 ST.addInitializer(2, function () {
 
 	const TARGET_SELECTOR = '.stile';
-	const TARGET_SELECTOR_ANCHOR = '.stile-anchor';
-	const TARGET_SELECTOR_ANCHOR_EXTERNAL = '.stile-anchor-external';
 
-	const PERMITTED_CLASSES = ['alignleft', 'aligncenter', 'alignright', 'size-thumbnail', 'size-small', 'size-medium', 'size-medium_large', 'size-large', 'size-full'];
-
-	modifySpanStyle();
-
-	let as = document.querySelectorAll(TARGET_SELECTOR + ' a');
-	modifyAnchorStyle(as);
-	as = document.querySelectorAll(TARGET_SELECTOR_ANCHOR + ' a');
-	modifyAnchorStyle(as);
-	as = document.querySelectorAll(TARGET_SELECTOR_ANCHOR_EXTERNAL + ' a');
-	modifyAnchorStyleExternal(as);
-
+	const spans = document.querySelectorAll(TARGET_SELECTOR + ' span');
+	modifySpanStyle(spans);
 	const fs = document.querySelectorAll(TARGET_SELECTOR + ' iframe');
 	modifyIframeStyle(fs);
 	const figs = document.querySelectorAll(TARGET_SELECTOR + ' figure');
@@ -37,8 +26,7 @@ ST.addInitializer(2, function () {
 	// -------------------------------------------------------------------------
 	// Span Styles
 
-	function modifySpanStyle() {
-		const spans = document.querySelectorAll(TARGET_SELECTOR + ' span');
+	function modifySpanStyle(spans) {
 		for (let i = 0; i < spans.length; i += 1) {
 			const target = spans[i];
 			let type = target.style.textDecorationLine;
@@ -55,119 +43,6 @@ ST.addInitializer(2, function () {
 				}
 			}
 		}
-	}
-
-
-	// -------------------------------------------------------------------------
-	// Anchor Styles
-
-	function modifyAnchorStyle(as) {
-		for (let i = 0; i < as.length; i += 1) {
-			const a = as[i];
-			if (isImageLink(a)) {
-				ST.addStile(a, 'link-image');
-				continue;
-			}
-			if (isEmpty(a)) {
-				const url = a.getAttribute('href');
-				if (isExternal(url)) ST.addStile(a, 'link-external');
-				continue;
-			}
-			if (!isSimple(a)) continue;
-			ST.addStile(a, 'link-simple');
-			const url = a.getAttribute('href');
-			if (isUrlLink(a, url)) {
-				ST.addStile(a, 'link-url');
-			}
-			if (isExternal(url)) {
-				ST.addStile(a, 'link-external');
-			} else if (isAnchor(url)) {
-				ST.addStile(a, 'link-anchor');
-			}
-		}
-	}
-
-	function modifyAnchorStyleExternal(as) {
-		for (let i = 0; i < as.length; i += 1) {
-			const a = as[i];
-			const url = a.getAttribute('href');
-			if (isExternal(url)) {
-				ST.addStile(a, 'link-external');
-			}
-		}
-	}
-
-	function isAnchor(url) {
-		const pos = url.indexOf('#');
-		if (pos === -1) return false;
-		const id = url.substr(pos + 1);
-		const tar = document.getElementById(id);
-		return tar !== null;
-	}
-
-	function isExternal(url) {
-		if (url === null || url === '') {
-			return false;
-		} else if (url.indexOf(location.protocol + '//' + location.host) === 0) {
-			return false;
-		} else if (url.match(/^https?:\/\//)) {
-			return true;
-		}
-		return false;
-	}
-
-	function isSimple(a) {
-		if (a.className) return false;
-		const cs = a.childNodes;
-		if (cs.length === 0) return false;
-		for (let i = 0; i < cs.length; i += 1) {
-			if (cs[i].className) return false;
-		}
-		for (let i = 0; i < cs.length; i += 1) {
-			const tn = cs[i].tagName;
-			if (tn === 'BR') continue;
-			if (tn && isInlineElement(cs[i])) continue;
-			if (tn) return false;
-		}
-		return true;
-	}
-
-	function isEmpty(a) {
-		if (a.className) return false;
-		const cs = a.childNodes;
-		return (cs.length === 0);
-	}
-
-	function isInlineElement(elm) {
-		const d = getComputedStyle(elm).display;
-		return d.indexOf('inline') !== -1;
-	}
-
-	function isUrlLink(a, url) {
-		const cs = a.childNodes;
-		if (cs.length === 0) return false;
-		return a.innerHTML.trim() === url;
-	}
-
-	function isImageLink(a) {
-		if (a.className) {
-			const cs = a.className.split(' ');
-			for (let i = 0; i < cs.length; i += 1) {
-				if (PERMITTED_CLASSES.indexOf(cs[i]) === -1) return false;
-			}
-		}
-		const cs = a.childNodes;
-		if (cs.length === 0) return false;
-		let success = false;
-		for (let i = 0; i < cs.length; i += 1) {
-			const tn = cs[i].tagName;
-			if (success === false && tn === 'IMG') {
-				success = true;
-				continue;
-			}
-			if (tn) return false;
-		}
-		return success;
 	}
 
 
