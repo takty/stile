c0261fb5fd9511146463e4fc5441bbd46d27617e
Takuto Yanagida <yanagida@stxst.com>
2019-06-06 10:17:04 +0900

    Refactor and add resize and scroll event handling

--- src/js/basic/base.js
+++ src/js/basic/base.js
@@ -3,7 +3,7 @@
  * Base Functions
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2019-06-05
+ * @version 2019-06-06
  *
  */
 
@@ -11,137 +11,116 @@
 window.ST = window['ST'] || {};
 
 
-// -----------------------------------------------------------------------------
+(function (NS) {
+
+	const initLevels = [[], [], [], [], [], [], [], []];
+
+	document.addEventListener('DOMContentLoaded', () => {
+		for (let inits of initLevels) {
+			for (let i = 0; i < inits.length; i += 1) inits[i]();  // Do not use 'for-of' here for IE
+		}
+	});
+
+	NS.addInitializer = (level, fn) => { initLevels[level].push(fn); }
 
 
-const _initializer = [[], [], [], [], [], [], [], []];
-
-document.addEventListener('DOMContentLoaded', function () {
-	for (let i = 0; i < _initializer.length; i += 1) {
-		const is = _initializer[i];
-		for (let j = 0; j < is.length; j += 1) is[j]();
-	}
-});
-
-ST.addInitializer = function (level, fn) {
-	_initializer[level].push(fn);
-}
+	// -------------------------------------------------------------------------
 
 
-// -----------------------------------------------------------------------------
+	NS.addStile = (elm, style) => {
+		if (elm.dataset.stile) {
+			const ssl = ' ' + elm.dataset.stile + ' ';
+			const sbb = ' ' + style + ' ';
+			if (ssl.indexOf(sbb) !== -1) return;
+			elm.dataset.stile = elm.dataset.stile + ' ' + style;
+		} else {
+			elm.dataset.stile = style;
+		}
+		// eslint-disable-next-line no-self-assign
+		elm.className = elm.className;  // Hack for IE11
+		if (!elm.className) elm.removeAttribute('class');
+	};
 
-
-ST.addStile = function (elm, style) {
-	if (elm.dataset.stile) {
+	NS.containStile = (elm, style) => {
+		if (!elm.dataset.stile) return false;
 		const ssl = ' ' + elm.dataset.stile + ' ';
 		const sbb = ' ' + style + ' ';
-		if (ssl.indexOf(sbb) !== -1) return;
-		elm.dataset.stile = elm.dataset.stile + ' ' + style;
-	} else {
-		elm.dataset.stile = style;
-	}
-	elm.className = elm.className;  // Hack for IE11
-	if (!elm.className) elm.removeAttribute('class');
-};
-
-ST.containStile = function (elm, style) {
-	if (!elm.dataset.stile) return false;
-	const ssl = ' ' + elm.dataset.stile + ' ';
-	const sbb = ' ' + style + ' ';
-	return (ssl.indexOf(sbb) !== -1);
-};
-
-ST.removeStile = function (elm, style) {
-	if (!elm.dataset.stile) return;
-	const ssl = ' ' + elm.dataset.stile + ' ';
-	const sbb = ' ' + style + ' ';
-	elm.dataset.stile = (ssl.replace(sbb, ' ')).trim();
-	elm.className = elm.className;  // Hack for IE11
-	if (!elm.className) elm.removeAttribute('class');
-};
-
-
-// -----------------------------------------------------------------------------
-
-
-ST.throttle = (fn) => {
-	let isRunning, that, args;
-
-	const run = () => {
-		isRunning = false;
-		fn.apply(that, args);
+		return (ssl.indexOf(sbb) !== -1);
 	};
-	return (...args) => {
-		that = this;
-		if (isRunning) return;
-		isRunning = true;
-		requestAnimationFrame(run);
+
+	NS.removeStile = (elm, style) => {
+		if (!elm.dataset.stile) return;
+		const ssl = ' ' + elm.dataset.stile + ' ';
+		const sbb = ' ' + style + ' ';
+		elm.dataset.stile = (ssl.replace(sbb, ' ')).trim();
+		// eslint-disable-next-line no-self-assign
+		elm.className = elm.className;  // Hack for IE11
+		if (!elm.className) elm.removeAttribute('class');
 	};
-};
 
-const _onResize = [];
-const _onScroll = [];
 
-document.addEventListener('DOMContentLoaded', () => {
-	window.addEventListener('resize', () => {
-		for (let i = 0; i < _onResize.length; i += 1) _onResize[i]();
+	// -------------------------------------------------------------------------
+
+
+	const resizeListeners = [];
+	const scrollListeners = [];
+
+	document.addEventListener('DOMContentLoaded', () => {
+		window.addEventListener('resize', () => { for (let l of resizeListeners) l(); });
+		window.addEventListener('scroll', () => { for (let l of scrollListeners) l(); });
 	});
-	window.addEventListener('scroll', () => {
-		for (let i = 0; i < _onScroll.length; i += 1) _onScroll[i]();
-	});
-});
 
-ST.onResize = (fn) => {
-	_onResize.push(ST.throttle(fn));
-};
+	NS.onResize = (fn) => { resizeListeners.push(NS.throttle(fn)); };
 
-ST.onScroll = (fn) => {
-	_onScroll.push(ST.throttle(fn));
-};
+	NS.onScroll = (fn) => { scrollListeners.push(NS.throttle(fn)); };
 
-
-// -----------------------------------------------------------------------------
-
-
-ST.elementTopOnWindow = function (elm) {
-	const br = elm.getBoundingClientRect();
-	return br.top + window.pageYOffset;
-};
-
-ST.elementLeftOnWindow = function (elm) {
-	const br = elm.getBoundingClientRect();
-	return br.left + window.pageXOffset;
-};
+	NS.throttle = (fn) => {
+		let isRunning, that, args;
+		// eslint-disable-next-line func-style
+		const run = () => {
+			isRunning = false;
+			fn.apply(that, args);
+		};
+		return (...origArgs) => {
+			that = this;
+			args = origArgs;
+			if (isRunning) return;
+			isRunning = true;
+			requestAnimationFrame(run);
+		};
+	};
 
 
-// -----------------------------------------------------------------------------
+	// -------------------------------------------------------------------------
 
 
-ST.makeOffsetFunction = function (fixedElementClass, fixedTopClass) {
-	let elmFixed = document.getElementsByClassName(fixedElementClass);
-	if (elmFixed && elmFixed.length > 0) {
-		elmFixed = elmFixed[0];
-		const elmTops = document.getElementsByClassName(fixedTopClass);
-		if (elmTops && elmTops.length > 0) {
-			return function () {
+	NS.makeOffsetFunction = (fixedElementClass, fixedTopClass) => {
+		let elmFixed = document.getElementsByClassName(fixedElementClass);
+		if (elmFixed && elmFixed.length > 0) {
+			elmFixed = elmFixed[0];
+			const elmTops = document.getElementsByClassName(fixedTopClass);
+			if (elmTops && elmTops.length > 0) {
+				return () => {
+					const pos = getComputedStyle(elmFixed).position;
+					if (pos === 'fixed') {
+						let height = 0;
+						for (let i = 0; i < elmTops.length; i += 1) height += elmTops[i].offsetHeight;
+						return height;
+					}
+					return 0;
+				};
+			}
+			return () => {
 				const pos = getComputedStyle(elmFixed).position;
-				if (pos === 'fixed') {
-					let height = 0;
-					for (let i = 0; i < elmTops.length; i += 1) height += elmTops[i].offsetHeight;
-					return height;
-				}
-				return 0;
+				return pos === 'fixed' ? elmFixed.offsetHeight : 0;
 			};
 		}
-		return function () {
-			const pos = getComputedStyle(elmFixed).position;
-			return pos === 'fixed' ? elmFixed.offsetHeight : 0;
-		};
-	}
-	return function () { return 0; }
-};
+		return () => 0;
+	};
 
-ST.getWpAdminBarHeight = function () {
-	const wpab = document.getElementById('wpadminbar');
-	return (wpab && getComputedStyle(wpab).position === 'fixed') ? wpab.offsetHeight : 0;
-};
+	NS.getWpAdminBarHeight = () => {
+		const wpab = document.getElementById('wpadminbar');
+		return (wpab && getComputedStyle(wpab).position === 'fixed') ? wpab.offsetHeight : 0;
+	};
+
+})(window.ST);
