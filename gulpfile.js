57b7b255ec12bfc259bb8a51e91dfe6fa5549b3b
Takuto Yanagida <yanagida@stxst.com>
2019-06-06 10:13:45 +0900

    Separate babel settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,41 @@
+/* eslint-disable no-undef */
 'use strict';
 
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')({ pattern: ['gulp-*'] });
 
 
-gulp.task('js-with-option', () => {
-	return gulp.src(['src/js/basic/*.js', 'src/js/content/*.js', 'src/js/option/**/*.js'])
-		.pipe($.plumber())
-		.pipe($.babel({ presets: [['@babel/env', { targets: { ie: 11 } }]] }))
-		.pipe($.concat('stile-full.min.js'))
-		.pipe($.uglify())
-		.pipe(gulp.dest('./dist/js'));
-});
+gulp.task('js-with-option', () => gulp.src(['src/js/basic/*.js', 'src/js/content/*.js', 'src/js/option/**/*.js'])
+	.pipe($.plumber())
+	.pipe($.babel())
+	.pipe($.concat('stile-full.min.js'))
+	.pipe($.uglify())
+	.pipe(gulp.dest('./dist/js'))
+);
 
-gulp.task('js-without-option', () => {
-	return gulp.src(['src/js/basic/*.js', 'src/js/content/*.js'])
-		.pipe($.plumber())
-		.pipe($.babel({ presets: [['@babel/env', { targets: { ie: 11 } }]] }))
-		.pipe($.concat('stile.min.js'))
-		.pipe($.uglify())
-		.pipe(gulp.dest('./dist/js'));
-});
+gulp.task('js-without-option', () => gulp.src(['src/js/basic/*.js', 'src/js/content/*.js'])
+	.pipe($.plumber())
+	.pipe($.babel())
+	.pipe($.concat('stile.min.js'))
+	.pipe($.uglify())
+	.pipe(gulp.dest('./dist/js'))
+);
 
-gulp.task('js-each', () => {
-	return gulp.src('src/js/**/*.js')
-		.pipe($.plumber())
-		.pipe($.babel({ presets: [['@babel/env', { targets: { ie: 11 } }]] }))
-		.pipe($.uglify())
-		.pipe($.rename({ extname: '.min.js' }))
-		.pipe(gulp.dest('./dist/js'));
-});
+gulp.task('js-each', () => gulp.src('src/js/**/*.js')
+	.pipe($.plumber())
+	.pipe($.babel())
+	.pipe($.uglify())
+	.pipe($.rename({ extname: '.min.js' }))
+	.pipe(gulp.dest('./dist/js'))
+);
 
 gulp.task('js', gulp.parallel('js-with-option', 'js-without-option', 'js-each'));
 
-gulp.task('sass', () => {
-	return gulp.src(['src/sass/**/*.scss'], { base: 'src/sass' })
-		.pipe($.plumber())
-		.pipe($.changed('./dist/sass'))
-		.pipe(gulp.dest('./dist/sass'));
-});
+gulp.task('sass', () => gulp.src(['src/sass/**/*.scss'], { base: 'src/sass' })
+	.pipe($.plumber())
+	.pipe($.changed('./dist/sass'))
+	.pipe(gulp.dest('./dist/sass'))
+);
 
 gulp.task('watch', () => {
 	gulp.watch('src/js/**/*.js', gulp.series('js'));
@@ -52,22 +49,20 @@ gulp.task('default', gulp.series('build', 'watch'));
 
 // -----------------------------------------------------------------------------
 
-gulp.task('docs-sass', gulp.series('sass', () => {
-	return gulp.src(['docs/style.scss', 'docs/reset.scss'])
-		.pipe($.plumber())
-		.pipe($.sourcemaps.init())
-		.pipe($.sass({ outputStyle: 'compressed' }))
-		.pipe($.autoprefixer({ browsers: ['ie >= 11'], remove: false }))
-		.pipe($.rename({ extname: '.min.css' }))
-		.pipe($.sourcemaps.write('.'))
-		.pipe(gulp.dest('./docs/css'));
-}));
+gulp.task('docs-sass', gulp.series('sass', () => gulp.src(['docs/style.scss', 'docs/reset.scss'])
+	.pipe($.plumber())
+	.pipe($.sourcemaps.init())
+	.pipe($.sass({ outputStyle: 'compressed' }))
+	.pipe($.autoprefixer({ browsers: ['ie >= 11'], remove: false }))
+	.pipe($.rename({ extname: '.min.css' }))
+	.pipe($.sourcemaps.write('.'))
+	.pipe(gulp.dest('./docs/css')))
+);
 
-gulp.task('docs-js', gulp.series('js-with-option', () => {
-	return gulp.src(['dist/js/stile-full.min.js'])
-		.pipe($.plumber())
-		.pipe(gulp.dest('./docs'));
-}));
+gulp.task('docs-js', gulp.series('js-with-option', () => gulp.src(['dist/js/stile-full.min.js'])
+	.pipe($.plumber())
+	.pipe(gulp.dest('./docs')))
+);
 
 gulp.task('docs-watch', () => {
 	gulp.watch('src/js/**/*.js',     gulp.series('docs-js'));
