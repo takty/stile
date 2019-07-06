49d192a8aa4d7a5f992ee74c4df6fba96077bf29
Takuto Yanagida <yanagida@stxst.com>
2019-07-06 15:35:06 +0900

    Minor change of caption

--- src/js/content/table-fixed-header.js
+++ src/js/content/table-fixed-header.js
@@ -65,7 +65,6 @@ window.ST = window['ST'] || {};
 		_create() {
 			this._head = this._createHeaderClone();
 			this._sbar = this._createScrollBarClone();
-			// this._ebtn = (!this._head || NS.containStile(this._table, ST_OPT_NO_ENLARGER)) ? null : this._createEnlargerButton();
 			this._ebtn = NS.containStile(this._table, ST_OPT_NO_ENLARGER) ? null : this._createEnlargerButton();
 			this._shade = this._createShade();
 
@@ -380,7 +379,7 @@ window.ST = window['ST'] || {};
 			if (capt) {
 				if (this._isScrollable()) {
 					if (this._stCapt) clearTimeout(this._stCapt);
-					this._stCapt = setTimeout(() => { capt.style.transform = `translateX(${sL}px)`; }, 20);
+					this._stCapt = setTimeout(() => { capt.style.transform = `translateX(${sL}px)`; }, 200);
 				} else {
 					capt.style.transform = null;
 				}
