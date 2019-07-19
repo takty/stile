e0ff1ee35dd72a6dffacc12aba4f3d3c62d66928
Takuto Yanagida <yanagida@stxst.com>
2019-07-19 19:24:38 +0900

    Minor change

--- src/js/option/pseudo-tab-page.js
+++ src/js/option/pseudo-tab-page.js
@@ -99,7 +99,7 @@ window.ST = window['ST'] || {};
 		updateVisibilityImmediately(tabUlss);
 		NS.onScroll(() => {
 			updateVisibility(tabUlss);
-			setTimeout(() => { focused = null; }, 200);
+			setTimeout(() => { focused = null; }, 400);
 		});
 	}
 
