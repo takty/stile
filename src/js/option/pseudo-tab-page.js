823762c3ffa77df3fb4ae6161820d4d236ab2c7e
Takuto Yanagida <yanagida@stxst.com>
2019-07-29 10:37:02 +0900

    Refactor and improve

--- src/js/option/pseudo-tab-page.js
+++ src/js/option/pseudo-tab-page.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 /**
  *
  * Pseudo Tab Page Classes (JS)
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2019-07-19
+ * @version 2019-07-29
  *
  */
 
@@ -19,68 +20,93 @@ window.ST = window['ST'] || {};
 	const ST_STATE_CURRENT    = 'current';
 	const SINGLE_TAB          = true;
 
+	const tabLineUls = [];
 	let focused = null;
+	let tlVs = null;
 
 	NS.addInit(4, () => {
-		const tabUlss = [];
 		const tps = document.querySelectorAll(SELECTOR_TARGET);
 		for (let i = 0; i < tps.length; i += 1) {
-			const tabUls = createTabPage(tps[i], i, tabUlss);
-			if (tabUls !== false) tabUlss.push(tabUls);
+			createTabPage(tps[i], i);
 		}
-		if (SINGLE_TAB) initializeSingleTab(tabUlss);
+		if (NS.assignAnchorOffset) NS.assignAnchorOffset(tabLineUls);
+		if (SINGLE_TAB) {
+			updateVisibilityImmediately();
+			NS.onIntersect((vs) => {
+				tlVs = vs;
+				updateVisibility();
+			}, true, { targets: tabLineUls, marginTop: 'OFFSET', threshold: 0 });
+		}
 	});
 
-	function createTabPage(container, idx, tabUlss) {
-		const fh = getFirstHeading(container);
-		if (!fh) return false;
-		const tabH = fh.tagName;
+	function createTabPage(container, idx) {
+		const [hs, htmls] = extractHeaders(container);
+		if (hs.length === 0) return false;
 
-		const cs = [].slice.call(container.children);
-		const hs = [];
+		const tls = [];
+		for (let i = 0; i < hs.length; i += 1) {
+			const tl = createTabLine(htmls, i, idx);
+			container.insertBefore(tl.ul, hs[i]);
+			tls.push(tl);
+			tabLineUls.push(tl.ul);
+		}
+		if (SINGLE_TAB) {
+			for (let i = 0; i < tls.length; i += 1) {
+				const tl = tls[i];
+				NS.addStile(tl.ul, 'hidden');
+				for (let j = 0; j < tl.as.length; j += 1) {
+					tl.as[j].addEventListener('click', () => { onTabClick(tls[j].ul); });
+				}
+			}
+		}
+	}
+
+	function createTabLine(htmls, tabIdx, contIdx) {
+		const ul = document.createElement('ul');
+		ul.id = ID_TAB_LIST_ID_BASE + (contIdx + 1) + '-' + (tabIdx + 1);
+		ul.className = CLS_TAB_LIST;
+		ul.classList.add('stile-link-target');
+
+		const ios = document.body.classList.contains('ios');
+		const stile = ios ? 'no-anchor-scroll' : 'anchor-scroll-fast';
+		const as = [];
+
+		for (let i = 0; i < htmls.length; i += 1) {
+			const a = document.createElement('a');
+			a.href = '#' + ID_TAB_LIST_ID_BASE + (contIdx + 1) + '-' + (i + 1);
+			a.innerHTML = htmls[i];
+			a.dataset['stile'] = stile;
+
+			const li = document.createElement('li');
+			li.appendChild(a);
+			ul.appendChild(li);
+			as.push(a);
+		}
+		NS.addStile(ul.children[tabIdx], ST_STATE_CURRENT);
+		return { ul, as };
+	}
+
+
+	// -------------------------------------------------------------------------
+
+
+	function extractHeaders(container) {
+		const fh = getFirstHeading(container);
+		if (!fh) return [[], []];
+		const tn = fh.tagName;
+
+		const hs    = [];
 		const htmls = [];
 
+		const cs = [].slice.call(container.children);
 		for (let i = 0; i < cs.length; i += 1) {
 			const c = cs[i];
-			if (c.tagName === tabH) {
+			if (c.tagName === tn) {
 				hs.push(c);
 				htmls.push(c.innerHTML);
 			}
 		}
-		const tabUls = [];
-		for (let i = 0; i < hs.length; i += 1) {
-			const tp = createTab(htmls, i, idx, tabUls, tabUlss);
-			container.insertBefore(tp.tabUl, hs[i]);
-			tabUls.push(tp.tabUl);
-		}
-		if (NS.assignAnchorOffset) NS.assignAnchorOffset(tabUls);
-		return tabUls;
-	}
-
-	function createTab(htmls, tabIdx, contIdx, tabUls, tabUlss) {
-		const tp = {};
-		tp.tabUl = document.createElement('ul');
-		tp.tabUl.id = ID_TAB_LIST_ID_BASE + (contIdx + 1) + '-' + (tabIdx + 1);
-		tp.tabUl.className = CLS_TAB_LIST;
-		tp.tabUl.classList.add('stile-link-target');
-		tp.tabAs = [];
-		const ios = document.body.classList.contains('ios');
-
-		for (let i = 0; i < htmls.length; i += 1) {
-			const li = document.createElement('li');
-			const tc = document.createElement('a');
-			tc.href = '#' + ID_TAB_LIST_ID_BASE + (contIdx + 1) + '-' + (i + 1);
-			tc.innerHTML = htmls[i];
-			tc.dataset['stile'] = ios ? 'no-anchor-scroll' : 'anchor-scroll-fast';
-			if (SINGLE_TAB) tc.addEventListener('click', () => { onTabClick(i, tabUls, tabUlss); });
-			li.appendChild(tc);
-
-			tp.tabUl.appendChild(li);
-			tp.tabAs.push(tc);
-		}
-		NS.addStile(tp.tabUl.children[tabIdx], ST_STATE_CURRENT);
-		if (SINGLE_TAB) NS.addStile(tp.tabUl, 'hidden');
-		return tp;
+		return [hs, htmls];
 	}
 
 	function getFirstHeading(container) {
@@ -95,66 +121,49 @@ window.ST = window['ST'] || {};
 	// -------------------------------------------------------------------------
 
 
-	function initializeSingleTab(tabUlss) {
-		updateVisibilityImmediately(tabUlss);
-		NS.onScroll(() => { updateVisibility(tabUlss); });
+	function onTabClick(clicked) {
+		focused = clicked;
+		updateVisibilityImmediately();
 	}
 
-	function onTabClick(idx, tabUls, tabUlss) {
-		focused = tabUls[idx];
-		updateVisibilityImmediately(tabUlss);
-	}
-
-	function updateVisibilityImmediately(tabUlss) {
-		for (let i = 0; i < tabUlss.length; i += 1) {
-			const tabUls = tabUlss[i];
-			for (let j = 0; j < tabUls.length; j += 1) {
-				NS.addStile(tabUls[j], 'immediately');
-			}
+	function updateVisibilityImmediately() {
+		for (let i = 0; i < tabLineUls.length; i += 1) {
+			NS.addStile(tabLineUls[i], 'immediately');
 		}
-		setTimeout(() => { updateVisibility(tabUlss) }, 10);
+		setTimeout(() => { updateVisibility() }, 10);
 		setTimeout(() => {
-			for (let i = 0; i < tabUlss.length; i += 1) {
-				const tabUls = tabUlss[i];
-				for (let j = 0; j < tabUls.length; j += 1) {
-					NS.removeStile(tabUls[j], 'immediately');
-				}
+			for (let i = 0; i < tabLineUls.length; i += 1) {
+				NS.removeStile(tabLineUls[i], 'immediately');
 			}
 			if (focused) {
 				const y = focused.getBoundingClientRect().top;
 				focused = null;
-				if (window.innerHeight < y) updateVisibility(tabUlss);
+				if (window.innerHeight < y) updateVisibility();
 			}
 		}, 1000);
 	}
 
-	function updateVisibility(tabUlss) {
-		for (let i = 0; i < tabUlss.length; i += 1) {
-			updateVisibilityOne(tabUlss[i]);
-		}
-	}
-
-	function updateVisibilityOne(tabUls) {
-		if (focused && tabUls.indexOf(focused) !== -1) {
-			for (let i = 0; i < tabUls.length; i += 1) {
-				const tabUl = tabUls[i];
-				if (tabUl === focused) {
-					NS.removeStile(tabUl, 'hidden');
+	function updateVisibility() {
+		if (focused && tabLineUls.indexOf(focused) !== -1) {
+			for (let i = 0; i < tabLineUls.length; i += 1) {
+				const t = tabLineUls[i];
+				if (t === focused) {
+					NS.removeStile(t, 'hidden');
 				} else {
-					NS.addStile(tabUl, 'hidden');
+					NS.addStile(t, 'hidden');
 				}
 			}
 			return;
 		}
 		let shown = false;
-		for (let i = 0; i < tabUls.length; i += 1) {
-			const tabUl = tabUls[i];
-			const y = tabUl.getBoundingClientRect().top;
-			if (0 < y && !shown) {
+		for (let i = 0; i < tabLineUls.length; i += 1) {
+			const t = tabLineUls[i];
+			const v = tlVs ? tlVs[i] : false;
+			if (v && !shown) {
 				shown = true;
-				NS.removeStile(tabUl, 'hidden');
+				NS.removeStile(t, 'hidden');
 			} else if (shown) {
-				NS.addStile(tabUl, 'hidden');
+				NS.addStile(t, 'hidden');
 			}
 		}
 	}
