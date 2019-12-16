524e5e3580127c2e9c1227805a77b51862a559ed
Takuto Yanagida <yanagida@stxst.com>
2019-12-16 15:47:07 +0900

    Refactoring

--- src/js/content/inline.js
+++ src/js/content/inline.js
@@ -53,26 +53,36 @@ window.ST = window['ST'] || {};
 	function modifyIframeStyle(fs) {
 		for (let i = 0; i < fs.length; i += 1) {
 			const f = fs[i];
-			let w = f.width, h = f.height;
-			if (!w || !h) {
-				const sW = f.style.width;
-				const sH = f.style.height;
-				if (sW.indexOf('px') !== sW.length - 2) continue;
-				if (sH.indexOf('px') !== sH.length - 2) continue;
-				w = parseInt(sW);
-				h = parseInt(sH);
-				if (!w || !h) continue;
-			}
-			const wrap = document.createElement('SPAN');
-			NS.addStile(wrap, 'iframe-wrapper');
-			const spacer = document.createElement('DIV');
-			spacer.style.paddingTop = (100 * h / w) + '%';
-			wrap.appendChild(spacer);
-			wrap.style.maxWidth = w + 'px';
-			f.parentElement.insertBefore(wrap, f);
-			wrap.appendChild(f);
-			wrap.dataset['aspect'] = Math.round(w / h * 1000) / 1000;
+			const size = extractIframeSize(f);
+			if (size) addIframeWrapper(f, size[0], size[1]);
 		}
+	}
+
+	function extractIframeSize(f) {
+		let w = parseInt(f.width);
+		let h = parseInt(f.height);
+		if (w && h) return [w, h];
+
+		const sW = f.style.width;
+		const sH = f.style.height;
+		if (sW.indexOf('px') !== sW.length - 2) return null;
+		if (sH.indexOf('px') !== sH.length - 2) return null;
+		w = parseInt(sW);
+		h = parseInt(sH);
+		if (w && h) return [w, h];
+		return null;
+	}
+
+	function addIframeWrapper(f, w, h) {
+		const wrap = document.createElement('SPAN');
+		NS.addStile(wrap, 'iframe-wrapper');
+		const spacer = document.createElement('DIV');
+		spacer.style.paddingTop = (100 * h / w) + '%';
+		wrap.appendChild(spacer);
+		wrap.style.maxWidth = w + 'px';
+		f.parentElement.insertBefore(wrap, f);
+		wrap.appendChild(f);
+		wrap.dataset['aspect'] = Math.round(w / h * 1000) / 1000;
 	}
 
 })(window.ST);
