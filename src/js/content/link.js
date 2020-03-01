4c4f543291b5a2d759b97ac5df4f416b12ad43e1
Takuto Yanagida <yanagida@stxst.com>
2020-03-01 13:53:13 +0900

    Improve url type detection

--- src/js/content/link.js
+++ src/js/content/link.js
@@ -3,7 +3,7 @@
  * Link Style (JS)
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2019-06-16
+ * @version 2020-03-01
  *
  * The function 'isExternalUrl' can be overwritten as follows:
  * <script>ST.isExternalUrl = function (url) { return true; }</script>
@@ -18,59 +18,62 @@ window.ST = window['ST'] || {};
 
 	const SEL_TARGET                 = '.stile';
 	const SEL_TARGET_ANCHOR          = '.stile-anchor';
-	const SEL_TARGET_ANCHOR_EXTERNAL = '.stile-anchor-external';
+	const SEL_TARGET_ANCHOR_URL      = '.stile-anchor-url';
+	const SEL_TARGET_ANCHOR_EXTERNAL = '.stile-anchor-external';  // For Compatibility
 
 	const PERMITTED_CLASSES = ['alignleft', 'aligncenter', 'alignright', 'size-thumbnail', 'size-small', 'size-medium-small', 'size-medium', 'size-medium-large', 'size-medium_large', 'size-large', 'size-full'];
 	const EXT_TABLE = { doc: 'word', docx: 'word', xls: 'excel', xlsx: 'excel', ppt: 'powerpoint', pptx: 'powerpoint', pdf: 'pdf' };
 
 	NS.isImageLink = isImageLink;  // Export the function
 	NS.addInit(1, () => {
-		let as = document.querySelectorAll(SEL_TARGET + ' a');
-		modifyAnchorStyle(as);
-		as = document.querySelectorAll(SEL_TARGET_ANCHOR + ' a');
-		modifyAnchorStyle(as);
-		as = document.querySelectorAll(SEL_TARGET_ANCHOR_EXTERNAL + ' a');
-		modifyAnchorStyleExternal(as);
+		modifyStyle(document.querySelectorAll(SEL_TARGET + ' a'));
+		modifyStyle(document.querySelectorAll(SEL_TARGET_ANCHOR + ' a'));
+		modifyStyleUrl(document.querySelectorAll(SEL_TARGET_ANCHOR_URL + ' a'));
+
+		// For Compatibility
+		modifyStyleUrl(document.querySelectorAll(SEL_TARGET_ANCHOR_EXTERNAL + ' a'));
 	});
 
 
 	// -------------------------------------------------------------------------
 
 
-	function modifyAnchorStyle(as) {
+	function modifyStyle(as) {
 		for (let i = 0; i < as.length; i += 1) {
 			const a = as[i];
 			if (isImageLink(a)) {
 				NS.addStile(a, 'link-image');
 				continue;
 			}
-			if (isEmpty(a)) {
-				const url = a.getAttribute('href');
+			const url = a.getAttribute('href');
+			if (isEmptyLink(a)) {
 				if (isExternal(url)) NS.addStile(a, 'link-external');
 				continue;
 			}
-			if (isSimple(a)) {
+			if (isSimpleLink(a)) {
 				NS.addStile(a, 'link-simple');
+				if (isUrlLink(a, url)) NS.addStile(a, 'link-url');
 
-				const url = a.getAttribute('href');
 				if (isAnchor(url)) {
 					NS.addStile(a, 'link-anchor');
-				} else {
-					if (isUrlLink(a, url)) NS.addStile(a, 'link-url');
-					if (isExternal(url)) NS.addStile(a, 'link-external');
+				} else if (isExternal(url)) {
+					NS.addStile(a, 'link-external');
 				}
 				addFileType(a, url);
 			}
 		}
 	}
 
-	function modifyAnchorStyleExternal(as) {
+	function modifyStyleUrl(as) {
 		for (let i = 0; i < as.length; i += 1) {
 			const a = as[i];
 			const url = a.getAttribute('href');
-			if (isExternal(url)) {
+			if (isAnchor(url)) {
+				NS.addStile(a, 'link-anchor');
+			} else if (isExternal(url)) {
 				NS.addStile(a, 'link-external');
 			}
+			addFileType(a, url);
 		}
 	}
 
@@ -100,13 +103,13 @@ window.ST = window['ST'] || {};
 		return success;
 	}
 
-	function isEmpty(a) {
+	function isEmptyLink(a) {
 		if (a.className) return false;
 		const cs = a.childNodes;
 		return (cs.length === 0);
 	}
 
-	function isSimple(a) {
+	function isSimpleLink(a) {
 		if (a.className) return false;
 		const cs = a.childNodes;
 		if (cs.length === 0) return false;
@@ -122,12 +125,9 @@ window.ST = window['ST'] || {};
 		return true;
 	}
 
-	function isAnchor(url) {
-		const pos = url.indexOf('#');
-		if (pos === -1) return false;
-		const id = url.substr(pos + 1);
-		const tar = document.getElementById(id);
-		return tar !== null;
+	function isInlineElement(elm) {
+		const d = getComputedStyle(elm).display;
+		return d.indexOf('inline') !== -1;
 	}
 
 	function isUrlLink(a, url) {
@@ -136,14 +136,24 @@ window.ST = window['ST'] || {};
 		return a.innerHTML.trim() === url;
 	}
 
-	function isInlineElement(elm) {
-		const d = getComputedStyle(elm).display;
-		return d.indexOf('inline') !== -1;
-	}
-
 
 	// -------------------------------------------------------------------------
 
+
+	function isAnchor(url) {
+		const pos = url.indexOf('#');
+		if (pos === -1) return false;
+		const id = url.substr(pos + 1);
+		const tar = document.getElementById(id);
+		if (tar === null) return false;
+		if (pos === 0) return true;
+		return url.substr(0, pos) === getUrlWithoutHash(location.href);
+	}
+
+	function getUrlWithoutHash(url) {
+		const p = url.indexOf('#');
+		return (p === -1) ? url : url.substr(0, p);
+	}
 
 	// Private function
 	function isExternal(url) { return NS.isExternalUrl(url); }
