1451f230a98f39ed50aa4436786e28703fb8c9e9
Takuto Yanagida <yanagida@stxst.com>
2022-03-24 21:51:42 +0900

    Fix gulp tasks

--- gulpfile.js/task-sass.js
+++ gulpfile.js/task-sass.js
@@ -3,7 +3,7 @@
  * Function for gulp (SASS)
  *
  * @author Takuto Yanagida
- * @version 2021-10-14
+ * @version 2022-03-23
  *
  */
 
@@ -13,7 +13,7 @@ const SASS_OUTPUT_STYLE = 'compressed';  // 'expanded' or 'compressed'
 
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const $ = require('gulp-load-plugins')({ pattern: ['gulp-*', '!gulp-sass'] });
+const $    = require('gulp-load-plugins')({ pattern: ['gulp-plumber', 'gulp-autoprefixer', 'gulp-rename', 'gulp-changed'] });
 
 const plumberOptions = {
 	errorHandler: function (err) {
@@ -25,7 +25,7 @@ const plumberOptions = {
 function makeSassTask(src, dest = './dist', base = null, addPostfix = true) {
 	const sassTask = () => gulp.src(src, { base: base, sourcemaps: true })
 		.pipe($.plumber(plumberOptions))
-		.pipe(sass({ outputStyle: SASS_OUTPUT_STYLE }))
+		.pipe(sass.sync({ outputStyle: SASS_OUTPUT_STYLE }))
 		.pipe($.autoprefixer({ remove: false }))
 		.pipe($.rename({ extname: addPostfix ? '.min.css' : '.css' }))
 		.pipe($.changed(dest, { hasChanged: $.changed.compareContents }))
